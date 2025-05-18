2c5eb23cf9f537e1c00093259cf7eacda4fe0124
sruthi madha <madhasruthi@gmail.com>
2025-05-18 17:51:16 +0530

    Fix: Ensure server.ts compiles to dist/server.js

---
+++ dist/server.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const bookRoutes_1 = __importDefault(require("./routes/bookRoutes"));
+const errorHandler_1 = require("./middleware/errorHandler");
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use((0, morgan_1.default)('dev'));
+app.use(express_1.default.json());
+// Root route
+app.get('/', (_req, res) => {
+    res.send('📚 API up. Use /api/books');
+});
+// Mount at /api/books
+app.use('/api/books', bookRoutes_1.default);
+app.use(errorHandler_1.errorHandler);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}/`);
+});
